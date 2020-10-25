7f3956159d77c55f8f6319d6ade23ff6e7cee5d9
cuzzuky87 <39122919+cuzzuky87@users.noreply.github.com>
2020-10-25 23:39:03 +0900

    スプレッド構文

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import "./styles.css";
 
-const data = {
-  name: "jak",
-  age: 28
-};
-
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
 <div>
@@ -13,3 +8,33 @@ document.getElementById("app").innerHTML = `
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
 `;
+
+/**
+ *
+ */
+const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん!`);
+sayHello();
+
+/**
+ * スプレッド構文
+ */
+
+const ary1 = [1, 2];
+console.log(ary1);
+console.log(...ary1);
+
+const sumFunc = (num1, num2) => console.log(num1 + num2);
+sumFunc(ary1[0], ary1[1]);
+sumFunc(...ary1);
+
+const ary2 = [1, 2, 3, 4, 5];
+const [num1, num2, ...ary3] = ary2;
+console.log(num1);
+console.log(num2);
+console.log(ary3);
+
+const ary4 = [10, 20];
+const ary5 = [30, 40];
+
+const ary6 = [...ary4, ...ary5];
+console.log(ary6);
